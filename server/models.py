c4fe90d2bcb0329e8f4b477f7e53f662823aa2d9
Spencer <eklund.spencer@outlook.com>
2023-10-16 10:47:18 -0600

    added model skeletons

--- server/models.py
+++ server/models.py
@@ -3,4 +3,55 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Grill(db.Model, SerializerMixin):
+    __tablename__ = 'grills'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    # one-to-many relationship with reviews, might need to have it's own model called grill_reviews
+
+    def __repr__(self):
+        return f'<Grill {self.name}>'
+    
+class Accessory(db.Model, SerializerMixin):
+    __tablename__ = 'accessories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    # one-to-many relationship with reviews, might need to have it's own model called accessory_reviews
+
+
+    def __repr__(self):
+        return f'<Accessory {self.name}>'
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    # one-to-many relationship with grills
+    # one-to-many relationship with accessories
+
+
+    def __repr__(self):
+        return f'<Review>'
+    
+
+class Location(db.Model, SerializerMixin):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String)
+
+    # many-to-many relationship with grills
+    # many-to-many relationship with accessories
+
+    def __repr__(self):
+        return f'<Location {self.address}>'
