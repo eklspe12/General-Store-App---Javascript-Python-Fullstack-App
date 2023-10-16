cdcc39d2bbb23392df056fe6b07cf3c94309e922
Spencer <eklund.spencer@outlook.com>
2023-10-16 14:55:51 -0600

    added route to connect delete from both ends

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
 import {Link, Switch, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Store from "./components/Store";
+
 
 function App() {
 
 const [grills, setGrills] = useState([])
 
 useEffect (() => {
-  fetch('whatever the url is')
-  .then((r) =>r.json())
+  fetch("http://127.0.0.1:5555/grills")
+  .then(r => {
+    if (r.ok) {
+      return r.json();
+    }
+    throw r;
+  })
   .then((grills) => {
     setGrills(grills);
   })
 }, []);
+
+function handleDeleteGrill(id) {
+  fetch(`http://127.0.0.1:5555/grills/${id}`, {
+  method: "DELETE", 
+  })
+  .then((r) => r.json())
+  .then(() => {
+    const updatedGrills = grills.filter((g) => g.id !== id);
+    setGrills(updatedGrills)
+  });
+}
 
 return (<>
   <NavBar/>
