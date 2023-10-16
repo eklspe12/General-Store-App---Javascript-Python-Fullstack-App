c8a4df70a12b2ba88d84008edfe980e887401848
eklspe12 <123013923+eklspe12@users.noreply.github.com>
2023-10-16 14:05:26 -0600

    Create NavBar.js

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+return (
+    <nav className="nav">
+       <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/store">Store</Link>
+    
+    </ul>
+    
+    </nav>
+   
+)
+
+}
+
+export default NavBar
