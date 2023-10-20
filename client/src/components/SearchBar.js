580345a7d296ef2e580f85f281875dffb114634b
Spencer <eklund.spencer@outlook.com>
2023-10-20 12:40:22 -0600

    fully functional

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -5,20 +5,19 @@ import * as Yup from 'yup';
 import AddLocation from "./AddLocation";
 
 function SearchBar() {
-  const [search, setSearch] = useState("");
+  // const [search, setSearch] = useState("");
   const [stocks, setStocks] = useState([]);
-  const [originalStocks, setOriginalStocks] = useState([])
-  const [searchMade, setSearchMade] = useState(false)
+  const [searchedTerm, setSearchedTerm] = useState("")
 
   const formik = useFormik({
     initialValues:{
-        search:search,
+        search:"",
     },
     validationSchema:Yup.object({
         search:Yup.string().required('Please enter search term')
     }),
     onSubmit: (values) => {
-        handleSearch(values.search);
+       setSearchedTerm(values.search)
     },
 });
 
@@ -29,40 +28,17 @@ function SearchBar() {
       .then((r) => r.json())
       .then((stocks) => {
         setStocks(stocks)
-        setOriginalStocks(stocks)
         console.log(stocks)} )
   
   }, []);
+//checkpoint
+const filteredStocks = stocks.filter((stock) => {
+  const productName = stock.product.name.toLowerCase();
+  const locationAddress = stock.location.address.toLowerCase();
+  const query = searchedTerm.toLowerCase();
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    setSearchMade(true);
-
-    if (search.trim() === "") {
-      setStocks(originalStocks);
-      return;
-    }
-
-    const filteredStocks = originalStocks.filter((stock) => {
-      const productName = stock.product.name.toLowerCase();
-      const locationAddress = stock.location.address.toLowerCase();
-      const query = search.toLowerCase();
-
-      return productName.includes(query) || locationAddress.includes(query);
-    });
-    setStocks(filteredStocks)
-
-  }
-
-  const handleChange = (e) => {
-    setSearch(e.target.value, () =>{
-    // console.log(search)
-    });
-  }
-  
-  useEffect(() => {
-    console.log(search);
-  }, [search]);
+  return productName.includes(query) || locationAddress.includes(query);
+});
 
   return (
     <div>
@@ -72,7 +48,7 @@ function SearchBar() {
            <div className="addLocationContaienr"><AddLocation/></div> 
            </div>
       <form onSubmit={formik.handleSubmit} className='stockSearch' >
-                <input name="name" type="text" className='searchInput' placeholder='Search available items...' value={formik.values.search} onChange={formik.handleChange}></input>
+                <input name="search" type="text" className='searchInput' placeholder='Search available items...' value={formik.values.search} onChange={formik.handleChange} />
                 {formik.touched.search && formik.errors.search ? (
                     <div className='searchError'>{formik.errors.search}</div>
                 ) : null }
@@ -81,7 +57,7 @@ function SearchBar() {
             </form>
 
       <ul className="searchUl">
-        {stocks.map((stock) => (
+        {filteredStocks.map((stock) => (
           <li className="stocks" key={stock.id}>
             <p>{stock.product ? stock.product.name : 'N/A'}, Address:{stock.location ? stock.location.address : 'N/A'}, Quantity: {stock.quantity}</p>
           </li>
--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -1,57 +1,88 @@
-import ProductList from './ProductList'
+import ProductList from './ProductList';
 import AddProduct from './AddProduct';
 import AddLocation from './AddLocation';
-import React, {useEffect} from 'react';
-import {useFormik} from 'formik';
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
-const Store = ({products, handleSearch, handleChange, search, searchMade, setProducts}) => {
-    const formik = useFormik({
-        initialValues:{
-            search:search,
-        },
-        validationSchema:Yup.object({
-            search:Yup.string().required('Please enter search term')
-        }),
-        onSubmit: (values) => {
-            handleSearch(values.search);
-        },
-    });
+const Store = ({ products, handleSearch, search, searchMade, setProducts }) => {
+	const formik = useFormik({
+		initialValues: {
+			search: search,
+		},
+		validationSchema: Yup.object({
+			search: Yup.string().required('Please enter search term'),
+		}),
+		onSubmit: (values) => {
+			handleSearch(values.search);
+		},
+	});
 
+	function handleDelete(productToDelete) {
+		const updatedProducts = products.filter((p) => p.id !== productToDelete.id);
+		setProducts(updatedProducts);
+	}
 
-    function handleDelete (productToDelete) {
-        const updatedProducts = products.filter((p) => 
-        p.id !== productToDelete.id);
-        setProducts(updatedProducts)
-    }
+	function updateProduct(updatedProduct) {
+		const updatedProducts = products.map((product) => {
+			if (product.id != updatedProduct.id) {
+				return product;
+			} else {
+				return updatedProduct;
+			}
+		});
+		setProducts(updatedProducts);
+	}
 
-    useEffect(() => {
-        document.title = 'Products';
-      }, []);
+	useEffect(() => {
+		document.title = 'Products';
+	}, []);
 
+	return (
+		<div className="searchBG">
+			<div className="searchContainer">
+				<form onSubmit={formik.handleSubmit} className="searchBar">
+					<input
+						name="search"
+						type="text"
+						className="searchInput"
+						placeholder="Search available items..."
+						value={formik.values.search}
+						onChange={formik.handleChange}
+					></input>
+					{formik.touched.search && formik.errors.search ? (
+						<div className="searchError">{formik.errors.search}</div>
+					) : null}
 
-    return (
-        <div className='searchBG'>
-            <div className='searchContainer'>
-            <form onSubmit={formik.handleSubmit} className='searchBar'>
-                <input name="search" type="text" className='searchInput' placeholder='Search available items...' value={formik.values.search} onChange={formik.handleChange}></input>
-                {formik.touched.search && formik.errors.search ? (
-                    <div className='searchError'>{formik.errors.search}</div>
-                ) : null }
-               
-                <input className='searchBtn' type="submit" value="Search products."></input>
-            </form>
-            </div>
-            <AddProduct products={products} setProducts={setProducts} />
-            
-            <div className='gridWrapper'>
-                <div className='storeContainer'>
-                    {(searchMade && products.length === 0) ? (<p className="searchError">No result's found, please check spelling.</p>) : (<div><ProductList products={products} onDelete={handleDelete} setProducts={setProducts}/></div>)}</div>
-            </div>
-        </div>
-    )
-}
+					<input
+						className="searchBtn"
+						type="submit"
+						value="Search products."
+					></input>
+				</form>
+			</div>
+			<AddProduct products={products} setProducts={setProducts} />
 
-export default Store
+			<div className="gridWrapper">
+				<div className="storeContainer">
+					{searchMade && products.length === 0 ? (
+						<p className="searchError">
+							No result's found, please check spelling.
+						</p>
+					) : (
+						<div>
+							<ProductList
+								products={products}
+								updateProduct={updateProduct}
+								onDelete={handleDelete}
+								setProducts={setProducts}
+							/>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-
+export default Store;
